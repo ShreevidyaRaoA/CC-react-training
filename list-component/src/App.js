f43fb662f17d4ef0d11585bf3cb14b8542c13e6a
ShreevidyaRaoA <shreevidyaraoa92@gmail.com>
2021-09-29 09:15:34 +0530

    Updating assignment6

--- list-component/src/App.js
+++ list-component/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 import List_Items from './components_list/List_Items'
+import GroceryList from './components_list/GroceryList'
+import Form from './components_list/Form'
 class App extends Component {
   render() {
   return (
