0b174af596604491bc9ffd9efec69fdc652d4010
ShreevidyaRaoA <shreevidyaraoa92@gmail.com>
2021-10-02 00:15:34 +0530

    Assignment7

---
+++ dummystates/src/App.js
@@ -0,0 +1,17 @@
+//import logo from './logo.svg';
+import './App.css';
+import DummyStates from "./DummyStates";
+
+import styles from "./DummyStates.module.css";
+// import SearchList from "./searching/SearchList";
+// import HackerStories from "./hackerstories/HackerStories";
+// import styles from "./hackerstories/PropsList.module.css";
+
+function App() {
+    return (
+      <div>
+   <DummyStates/>
+   </div>
+    );
+}
+export default App;
---
+++ dummystates/src/DummyStates.js
@@ -0,0 +1,29 @@
+import styles from './DummyStates.module.css';
+import DummyStates from './dummystates/DummyStates';
+
+const SearchList = () => {
+    let serachNumber ="";
+}
+const handlePhonenumberChange = (event) => {
+    searchNumber = event.target.value;
+};
+        
+        return (
+            <div>  
+            
+            
+                <label >Pnonenumber</label> 
+                <input id  =  "telephone" type = "tel" required  className = "form-input" maxlength="10" pattern="\d{10}" title="Please enter exactly 10 digits" pattern="[1-9]{1}[0-9]{9}" value = {this.state.phonenumber}
+                onChange = {handlePhonenumberChange}
+                />
+                <p> Searching for <strong>{searchNumber}</strong></p>
+           
+       
+         <button type="submit">Submit</button>
+         </div>  
+        );
+    
+    
+
+export default DummyStates;
+
